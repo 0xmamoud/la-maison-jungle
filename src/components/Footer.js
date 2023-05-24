8e766e873367156133332d1f19c4aa699d1a56bb
KANE <mahamadoudaba.kane@gmail.com>
2023-05-24 17:13:34 +0200

    fin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,33 @@
-import {useState} from "react";
+import { useState } from 'react'
 import '../styles/Footer.css'
 
-function handleBlur(inputValue) {
-    if (!inputValue.includes('@')) {
-      alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide.");
-    } 
-}
 function Footer() {
-    const [inputValue, setInputValue] = useState("");
+	const [inputValue, setInputValue] = useState('')
 
-    return (
-        <footer className="lmj-footer">
-            <div className="lmj-footer-elem">
-                Pour les passionné.e.s de plantes 🌿🌱🌵
-            </div>
-            <div className="lmj-footer-elem">
-                Laissez-nous votre mail:
-                <input type="text" value={inputValue} onChange={(e) => {
-                    setInputValue(e.target.value)
-                }} onBlur={() =>{
-                    handleBlur(inputValue);
-                }}
-                placeholder='Entrez votre mail' />
-            </div>
-        </footer>
-    )
+	function handleInput(e) {
+		setInputValue(e.target.value)
+	}
+
+	function handleBlur() {
+		if (!inputValue.includes('@')) {
+			alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥")
+		}
+	}
+
+	return (
+		<footer className='lmj-footer'>
+			<div className='lmj-footer-elem'>
+				Pour les passionné·e·s de plantes 🌿🌱🌵
+			</div>
+			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+			<input
+				placeholder='Entrez votre mail'
+				onChange={handleInput}
+				value={inputValue}
+				onBlur={handleBlur}
+			/>
+		</footer>
+	)
 }
 
-export default Footer;
+export default Footer
